503b7e72e53aa23bd760370dc2391f66fd93a400
Dipjyoti Roy <dipjyoti007@gmail.com>
2023-10-18 16:20:19 +0530

    Changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-  <h1>Home Page</h1>
-     
+      <h1>Home Page</h1>
     </main>
-  )
+  );
 }
